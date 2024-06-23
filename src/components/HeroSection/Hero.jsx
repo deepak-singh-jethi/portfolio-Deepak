a505a2107c095e85932a2ef01a9eaa51362c5647
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2024-06-23 10:58:57 +0530

    Update Hero.jsx

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           className="image-container mt-40 md:mt-0">
           <div className="liquid-border">
             <img
-              src="https://ibb.co/kJ7ZPSs"
+              src="https://i.ibb.co/ScjSZ34/32332349-811317055726429-5912480176133898240-n.jpg"
               alt="Your Image"
               className="image"
             />
